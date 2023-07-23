6464317d4eaa30d479979fdaec47a2e156b087e3
Rus Miller <lancemonotone@gmail.com>
2023-07-23 15:57:38 -0400

    Better ancestor page detection.

--- wp-content/themes/n1_durable_goods/lib/class.navigation.php
+++ wp-content/themes/n1_durable_goods/lib/class.navigation.php
@@ -1,41 +1,26 @@
 <?php namespace N1_Durable_Goods;
 
 class Navigation {
-	public function __construct() {
-		add_filter( 'nav_menu_css_class', [ $this, 'add_current_nav_ancestor_class' ], 10, 3 );
-	}
+    public function __construct() {
+        add_filter( 'nav_menu_css_class', [ $this, 'add_current_nav_ancestor_class' ], 10, 3 );
+    }
 
-	public function add_current_nav_ancestor_class( $classes, $item, $args ) {
-		if ( is_singular() ) {
-			global $post;
+    public function add_current_nav_ancestor_class( $classes, $item, $args ) {
+        $page_class = N1_Magazine::get_page_class();
 
-			// Get item url
-			$item_url = $item->url;
-			// Get last segment of item url
-			if ( preg_match( '~/([^/]+)(?=/[^/]*$)~', $item_url, $matches ) ) {
-				$item_page = $matches[1];
+        // Flatten the array of classes.
+        $body_class = join( ' ', $classes );
 
-				// Get current page
-				$page_class = N1_Magazine::get_page_class();
+        // Check if the page class is in the classes array.
+        // This is a bit of a fudge because Events is also Online
+        // Only, and has a class of 'events online-only', so we
+        // need to check for both.
+        if ( stristr( $body_class, $page_class ) ) {
+            $classes[] = 'current-ancestor';
+        }
 
-				// Massage the page class to match the taxonomy
-				$current_taxonomy = $page_class === 'magazine' ? 'category' : $page_class;
-
-				// get current post terms
-				$terms = get_the_terms( $post->ID, $current_taxonomy );
-
-				// check if current post belongs to current taxonomy
-				foreach ( $terms as $term ) {
-					if ( ! empty( $term->taxonomy ) && $term->taxonomy === $current_taxonomy && $item_page === $page_class ) {
-						$classes[] = 'current-ancestor';
-					}
-				}
-			}
-
-		}
-
-		return $classes;
-	}
+        return $classes;
+    }
 }
 
 new Navigation();
