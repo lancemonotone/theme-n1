a0b786bcc3004508b62353bf1e06ba10800ca355
Rus Miller <lancemonotone@gmail.com>
2023-11-11 16:35:08 -0500

    Moved issue-landing styles into _landing-issue.scss and updated.

--- wp-content/themes/n1_durable_goods/parts/loop-toc.php
+++ wp-content/themes/n1_durable_goods/parts/loop-toc.php
@@ -1,20 +1,20 @@
 <?php namespace N1_Durable_Goods;
 
 $context_issue = N1_Magazine::get_context_issue();
-$hero_img = get_field('issue_toc_image', $context_issue->ID);
+$hero_img      = get_field( 'issue_toc_image', $context_issue->ID );
 ?>
-<section class="magazine issue-landing hero" id="hero">
-	<figure id="magazine-issue-landing-hero" class="hero" style="background-image:url(<?php echo $hero_img['sizes']['hero-module']?>);"></figure>
-	<div class="hero-wrapper">
-		<div class="hero-cell">
-			<div class="hero category"><span class="module-hed"><?php echo $context_issue->post_title?></span></div>
-			<h2 class="hero title"><?php echo get_field('issue_name', $context_issue->ID)?></h2>
-			<p class="hero dek"><?php echo get_field('issue_dek', $context_issue->ID)?></p>
-		</div><!-- .hero-cell -->
-	</div><!-- .hero-wrapper -->
+<section class="hero">
+    <figure style="background-image:url(<?php echo $hero_img[ 'sizes' ][ 'hero-module' ] ?>);"></figure>
+    <div class="hero-cell">
+        <div class="hero-category">
+            <span class="module-hed"><?php echo $context_issue->post_title ?></span>
+        </div>
+        <h2 class="hero-title"><?php echo get_field( 'issue_name', $context_issue->ID ) ?></h2>
+        <p class="hero-dek"><?php echo get_field( 'issue_dek', $context_issue->ID ) ?></p>
+    </div><!-- .hero-cell -->
 </section>
 <div id="main" class="main cf">
-	<div class="content-wrapper">
-		<?php get_template_part( 'sidebars/sidebar', 'toc' )?>
-	</div><!-- .content-wrapper -->
+    <div class="archive-wrapper">
+        <?php get_template_part( 'sidebars/sidebar', 'toc' ) ?>
+    </div><!-- .content-wrapper -->
 </div><!-- #main -->
