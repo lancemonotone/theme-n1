5f2734920f84a80b20cb8e702b487598a433e9b7
Rus Miller <lancemonotone@gmail.com>
2023-11-19 13:46:24 -0500

    Removed a bunch of unused js libraries.

--- wp-content/themes/n1_durable_goods/js/main.js
+++ wp-content/themes/n1_durable_goods/js/main.js
@@ -10,26 +10,5 @@ function popitup(url) {
 		$('.subscribe-module .mm-form input[type=radio]').change(function(){
 			$(this).parents('.mm-form').find('a.mm-form-submit').attr('href', $(this).attr('value'));
 		});
-		if($('#issue-slider').length){
-		//	Responsive layout, resizing the items
-			$('#issue-slider').carouFredSel({
-				circular: true,
-				infinite: true,
-				auto: false,
-				responsive: true,
-				width: '90%',
-				//scroll: "page",
-				prev: '#prev3',
-				next: '#next3',
-				items: {
-					width: 120,
-					height: 185,	//	optionally resize item-height
-					visible: {
-						min: 5,
-						max: 8
-					}
-				}
-			});
-		}
 	});
 }(jQuery));
